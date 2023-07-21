1e0b7899d27562d9ad34a1cb899fd92159aa6a64
Abdul Hadi <hadi22905@gmail.com>
2023-07-21 16:47:26 +0500

    bug fixes

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -10,50 +10,44 @@ const page = ({ Single }) => {
 
   return (
     <Formate>
-      <section className="singleBlogMainSection" style={{marginTop:"120px"}}>
-      <div className="singleBlogSubMain">
-      <div className="" style={{display:"grid",justifyContent:"center",margin:"auto",padding:"20px 0"}}>
+      <section className="singleBlogMainSection" style={{ marginTop: "120px" }}>
+        <div className="singleBlogSubMain">
+          <div
+            className=""
+            style={{
+              display: "grid",
+              justifyContent: "center",
+              margin: "auto",
+              padding: "20px 0"
+            }}
+          >
             <Image
               src={Single.singleBlog.avatar}
               width={900}
               height={600}
-              style={{ maxWidth: "100%",borderRadius:"10px" }}
+              style={{ maxWidth: "100%", borderRadius: "10px" }}
             ></Image>
           </div>
-        <div className="">
-          <Author></Author>
+          <div className="">
+            <Author></Author>
+          </div>
+          <div className="post">
+            <h1 className="singlePostTtitle">{Single.singleBlog.title}</h1>
+            <p className="">{Single.singleBlog.subTitle}</p>
+
+            <div
+              dangerouslySetInnerHTML={createMarkup(Single.singleBlog.desc)}
+              className=""
+            ></div>
+          </div>
+          {/* <Related ></Related> */}
         </div>
-        <div className="post">
-          <h1 className="singlePostTtitle">
-            {Single.singleBlog.title}
-          </h1>
-          <p className="">
-            {Single.singleBlog.subTitle}
-          </p>
-         
-          <div
-            dangerouslySetInnerHTML={createMarkup(Single.singleBlog.desc)}
-            className=""
-          ></div>
-        </div>
-        {/* <Related ></Related> */}
-
-
-
-     
-
-
-
-
-
-
-
-
-      </div>
       </section>
 
-      <h1 className="font-bold text-4xl py-12" style={{padding:"20px"}}>Related</h1>
-      <div className="flex gap-5" style={{padding:"20px"}}>
+      <h1 className="font-bold text-4xl py-12" style={{ padding: "20px" }}>
+        Related
+      </h1>
+      <div className="flex gap-5" style={{ padding: "20px" }}>
         <div className="image flex flex-col justify-start">
           <Image
             src={Single.singleBlog.avatar}
@@ -61,24 +55,23 @@ const page = ({ Single }) => {
             width={250}
             height={400}
             alt={Single.singleBlog.avatarAlt}
-
           />
         </div>
         <div className="info flex justify-center flex-col">
           <div className="cat">
             <div className="text-orange-600 hover:text-orange-800">
-            {Single.singleBlog.category}
+              {Single.singleBlog.category}
             </div>
             <div className="text-gray-800 hover:text-gray-600">
-            {new Date(Single.singleBlog.createdAt).toDateString()}
+              {new Date(Single.singleBlog.createdAt).toDateString()}
             </div>
           </div>
           <div className="title">
             <Link
-              href={"/"}
+              href={`/posts/${v.slug}`}
               className="text-sm md:text-3xl font-bold text-gray-800 hover:text-gray-600 "
             >
-            {Single.singleBlog.title}
+              {Single.singleBlog.title}
             </Link>
           </div>
           {/* <Author></Author> */}
@@ -91,7 +84,9 @@ const page = ({ Single }) => {
 export default page;
 
 export async function getServerSideProps({ params }) {
-  const response = await fetch(`https://hadi-blogging-app.vercel.app/api/blog/${params.slug}`);
+  const response = await fetch(
+    `https://hadi-blogging-app.vercel.app/api/blog/${params.slug}`
+  );
   const Single = await response.json();
   return { props: { Single } };
 }
